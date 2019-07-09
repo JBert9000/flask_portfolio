6094030fb1c74e62814255d040f333416d15134f
John Bert <jbertlik@gmail.com>
2019-07-09 16:33:09 -0400

    commented out logging module since now none of my site works

--- python_portfolio.py
+++ python_portfolio.py
@@ -3,8 +3,8 @@ import logging
 
 app=Flask(__name__)
 
-app.logger.addHandler(logging.StreamHandler(sys.stdout))
-app.logger.setLevel(logging.ERROR)
+# app.logger.addHandler(logging.StreamHandler(sys.stdout))
+# app.logger.setLevel(logging.ERROR)
 
 @app.route('/plot/')
 def plot():
