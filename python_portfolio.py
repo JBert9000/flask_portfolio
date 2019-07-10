fef4906f350f0a27668a97ba0f7d07bcdd12793a
John Bert <jbertlik@gmail.com>
2019-07-10 12:06:51 -0400

    Finally got my stock chart working with Heroku. I decided to add another one even though the code isn't DRY. I will fix that later on.

--- python_portfolio.py
+++ python_portfolio.py
@@ -22,7 +22,15 @@ def plot():
 
     start=six_months
     end=datetime.datetime.now()
-    df=data.DataReader(name="AMD",data_source="yahoo",start=start,end=end)
+
+    stock_names=["AMD","MSFT"]
+
+    # def tickers(stock_names):
+    #     for i in stock_names:
+    #         return i
+
+
+    df=data.DataReader(name=stock_names[0],data_source="yahoo",start=start,end=end)
 
     def increase_decrease(c,o):
         if c > o:
@@ -40,7 +48,7 @@ def plot():
     df["Height"]=abs(df.Close-df.Open)
 
     p=figure(x_axis_type='datetime',width=1000,height=300,sizing_mode='scale_width') # can repalce responsive=True with: sizing_mode='scale_width'
-    p.title.text="Candlestick Chart"
+    p.title.text=stock_names[0]
     p.grid.grid_line_alpha=0.4
 
     hours_12=12*60*60*1000
@@ -56,10 +64,53 @@ def plot():
            hours_12,df.Height[df.Status=="Decrease"],fill_color="#FF1493",line_color="black")
 
     script1,div1=components(p)
+    script2,div2=components(p)
     cdn_js=CDN.js_files[0]
     cdn_css=CDN.css_files[0]
+
+##############
+
+    df2=data.DataReader(name=stock_names[1],data_source="yahoo",start=start,end=end)
+
+
+
+    def increase_decrease(c,o):
+        if c > o:
+            value="Increase"
+        elif c < o:
+            value="Decrease"
+        else:
+            value="Equal"
+
+        return value
+
+    df2["Status"]=[increase_decrease(c,o) for c,o in zip(df2.Close,df2.Open)]
+
+    df2["Middle"]=(df2.Open+df2.Close)/2
+    df2["Height"]=abs(df2.Close-df2.Open)
+
+    p2=figure(x_axis_type='datetime',width=1000,height=300,sizing_mode='scale_width') # can repalce responsive=True with: sizing_mode='scale_width'
+    p2.title.text=stock_names[1]
+    p2.grid.grid_line_alpha=0.4
+
+    #.segment method takes 4 manditory arguments;high of both x & y, and low of both x & y
+    p2.segment(df2.index,df2.High,df2.index,df2.Low,color="black")
+
+    #.rect is the rectangle method. Arguments are: rect(x axis, y axis, width, height)
+    p2.rect(df2.index[df2.Status=="Increase"],df2.Middle[df2.Status=="Increase"],
+           hours_12,df2.Height[df2.Status=="Increase"],fill_color="#00FA9A",line_color="black")
+
+    p2.rect(df2.index[df2.Status=="Decrease"],df2.Middle[df2.Status=="Decrease"],
+           hours_12,df2.Height[df2.Status=="Decrease"],fill_color="#FF1493",line_color="black")
+
+    script1,div1=components(p)
+    script2,div2=components(p2)
+    cdn_js=CDN.js_files[0]
+    cdn_css=CDN.css_files[0]
+
     # in this case, the render_template function takes 5 arguments; the page to be rendered, and the 4 variables that call the bokeh plot.
-    return render_template("plot.html",script1=script1,div1=div1,cdn_js=cdn_js,cdn_css=cdn_css)
+    return render_template("plot.html",script1=script1,div1=div1,script2=script2,div2=div2,cdn_js=cdn_js,cdn_css=cdn_css)
+
 
 @app.route('/')
 def home():
