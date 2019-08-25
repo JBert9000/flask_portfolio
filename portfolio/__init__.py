fa30ee9c06dafe3dd0c03f94c0b485336191ca6b
John Bert <jbertlik@gmail.com>
2019-08-25 10:40:28 -0400

    changed SQLAlchemy Databse URI to match Heroku's again.

--- portfolio/__init__.py
+++ portfolio/__init__.py
@@ -23,11 +23,11 @@ app.config['SECRET_KEY']='613cbb85c192d44ce3c5e5b66ef35110'
 app.logger.addHandler(logging.StreamHandler(sys.stdout))
 app.logger.setLevel(logging.ERROR)
 
-app.config['SQLALCHEMY_DATABASE_URI']='postgresql://zeta_g:postgres123@localhost/blog_posts'
+# app.config['SQLALCHEMY_DATABASE_URI']='postgresql://zeta_g:postgres123@localhost/blog_posts'
 
 # app.config['SQLALCHEMY_DATABASE_URI']='postgresql://zeta_g:postgres123@localhost/tetris_scores'
 
-# app.config['SQLALCHEMY_DATABASE_URI']='postgres://wwahvywfqyxtzh:f467af8693cb8a633a9307bf43adb8e25ae04b04593dd010770c59e1ec926c08@ec2-107-21-216-112.compute-1.amazonaws.com:5432/ddvtoqgimmm9b9?sslmode=require'
+app.config['SQLALCHEMY_DATABASE_URI']='postgres://wwahvywfqyxtzh:f467af8693cb8a633a9307bf43adb8e25ae04b04593dd010770c59e1ec926c08@ec2-107-21-216-112.compute-1.amazonaws.com:5432/ddvtoqgimmm9b9?sslmode=require'
 
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
