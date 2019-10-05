71ce5b9be76f2b9d02369d44db39fe922d293b70
John Bert <jbertlik@gmail.com>
2019-10-05 12:44:58 -0400

    changed the MY_CV path again

--- portfolio/config.py
+++ portfolio/config.py
@@ -11,12 +11,12 @@ class Config:
     MAIL_USERNAME = os.environ.get("YANDEX_EMAIL_USER")
     MAIL_PASSWORD = os.environ.get("YANDEX_EMAIL_PASS")
 
-    # SQLALCHEMY_DATABASE_URI = os.environ['DATABASE_URL']
+    SQLALCHEMY_DATABASE_URI = os.environ['DATABASE_URL']
 
-    SQLALCHEMY_DATABASE_URI = os.environ.get('SQLALCHEMY_DATABASE_URI_LOCAL')
+    # SQLALCHEMY_DATABASE_URI = os.environ.get('SQLALCHEMY_DATABASE_URI_LOCAL')
     # SQLALCHEMY_DATABASE_URI = os.environ["SQLALCHEMY_DATABASE_URI_PORTFOlIO"]
 
     # MY_CV = '/Users/Honzor/Desktop/PROGRAMMING/Python/projects/python_portfolio/mysite/demo/portfolio/static/client/pdf/'
-    MY_CV = 'demo/portfolio/static/client/pdf/'
+    MY_CV = 'portfolio/static/client/pdf/'
 
     SQLALCHEMY_TRACK_MODIFICATIONS = False
