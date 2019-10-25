acab2a02f5493a285b8b5a43adebf338eceb0a70
John Bert <jbertlik@gmail.com>
2019-10-25 06:19:52 -0400

    upgraded Pillow for security, and changed my CV slightly.

--- portfolio/tests/test_user.py
+++ portfolio/tests/test_user.py
@@ -3,18 +3,8 @@ import json
 from flask_sqlalchemy import SQLAlchemy
 from portfolio import create_app
 from portfolio.users.routes import users
-from flask import url_for
+from flask import url_for, Response
 
-
-# @pytest.fixture
-# def user():
-#     user = users
-#     return user
-
-
-# def test_register_route(app):
-#     response = app.get("/register/")
-#     assert response.status_code == 200
 
 def test_register_route(client):
     assert client.get(url_for('users.register')).status_code == 200
